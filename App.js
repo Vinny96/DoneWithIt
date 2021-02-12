26eb5f66f717ba3be9c122dfde231d50a99a0954
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-02-11 19:00:14 -0500

    Implemented some touchable code

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, TouchableHighlight} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text> Hello React Native </Text>
+      <TouchableHighlight>
+      <Image
+      source = {{
+        width:200,
+        height:300,
+        uri : "https://picsum.photos/200/300",
+      }}
+      />
+      </TouchableHighlight>
+    </SafeAreaView>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
