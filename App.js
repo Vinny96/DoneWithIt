0fe46bc7517fc102f8fcf3ec45bd4d50c9ab843d
Vinojen Gengatharan <gengatharanvinojen@gmail.com>
2021-02-21 18:49:49 -0500

    Alert functionality with butons

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, TouchableHighlight} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, TouchableOpacity, TouchableHighlight, Button, Alert} from 'react-native';
 
 export default function App() {
   return (
@@ -14,7 +14,16 @@ export default function App() {
         uri : "https://picsum.photos/200/300",
       }}
       />
-      </TouchableHighlight>
+       </TouchableHighlight>
+       <Button title = "Click me"
+        onPress = {() => Alert.alert("This is an alert", "Giggity Giggity 50's Giggity", [
+        {text : "Yes", onPress: () => console.log("Yes")},
+        {text : "No", onPress: () => console.log("No")}, 
+        {text : "Giggity", onPress: () => console.log("Giggity Giggity 50's Giggity")
+      }])}
+       
+       
+       />
     </SafeAreaView>
 
   );
